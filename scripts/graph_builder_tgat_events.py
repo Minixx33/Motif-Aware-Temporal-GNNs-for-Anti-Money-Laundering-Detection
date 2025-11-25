a0327e8694e2d746d1d9c1283c1642b9995c6c8a
Minix33 <yasminetohamy@gmail.com>
2025-11-25 17:35:35 +0400

    RAT_low tgat graph

--- scripts/graph_builder_tgat_events.py
+++ scripts/graph_builder_tgat_events.py
@@ -36,10 +36,10 @@ import torch
 BASE_DIR = r"C:\Users\yasmi\OneDrive\Desktop\Uni - Master's\Fall 2025\MLR 570\Motif-Aware-Temporal-GNNs-for-Anti-Money-Laundering-Detection\ibm_transcations_datasets"
 
 # Baseline
-DATASET = "HI-Small_Trans.csv"
+# DATASET = "HI-Small_Trans.csv"
 
 # RAT examples:
-# DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_low.csv")
+DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_low.csv")
 # DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_medium.csv")
 # DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_high.csv")
 
