4bdaad4ca8392c244d322c371b73a4007a67c2a1
Minix33 <yasminetohamy@gmail.com>
2025-11-25 17:03:33 +0400

    RAT_medium graph

--- scripts/graph_builder_static.py
+++ scripts/graph_builder_static.py
@@ -47,7 +47,7 @@ BASE_DIR = r"C:\Users\yasmi\OneDrive\Desktop\Uni - Master's\Fall 2025\MLR 570\Mo
 # DATASET = "HI-Small_Trans.csv"
 
 # RAT (Routine Activity Theory):
-DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_low.csv")
+# DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_low.csv")
 # DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_medium.csv")
 # DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_high.csv")
 
