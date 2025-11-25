b7b0f66fd0524fe202973d3c7e1078d10d487e0b
Minix33 <yasminetohamy@gmail.com>
2025-11-25 17:38:35 +0400

    RAT_medium tgat graph

--- scripts/graph_builder_tgat_events.py
+++ scripts/graph_builder_tgat_events.py
@@ -39,9 +39,9 @@ BASE_DIR = r"C:\Users\yasmi\OneDrive\Desktop\Uni - Master's\Fall 2025\MLR 570\Mo
 # DATASET = "HI-Small_Trans.csv"
 
 # RAT examples:
-DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_low.csv")
+# DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_low.csv")
 # DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_medium.csv")
-# DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_high.csv")
+DATASET = os.path.join("RAT", "HI-Small_Trans_RAT_high.csv")
 
 # SLT examples:
 # DATASET = os.path.join("SLT", "HI-Small_Trans_SLT_low.csv")
